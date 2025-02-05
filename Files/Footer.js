bfc9b3e6a9a09f3e6b839da565714433e6ae42b0
Ida Modigh <modigida@hotmail.com>
2025-02-05 21:13:29 +0100

    Add Footer.html component, display with JS on all pages.

---
+++ Files/Footer.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const footerHTML = `
+        <footer class="footer">
+            <p>&copy; Bjurslätts Bok Butik 2025</p>
+        </footer>
+    `;
+    document.getElementById('footer-placeholder').innerHTML = footerHTML;
+});
